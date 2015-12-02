23c43cc7cbe366a92870b61f9e416472973f3c1c
David Stancu <dstancu@nyu.edu>
2015-12-02 17:19:48 -0500

    add longer token for more fun

--- config/initializers/dwolla.rb
+++ config/initializers/dwolla.rb
@@ -2,7 +2,7 @@ require 'dwolla_swagger'
 
 module Dwolla
   DwollaSwagger::Swagger.configure do |config|
-    config.access_token = 'HcFb6DMNZvFyeW5hS9bQnbXrUaTmg0ifdGlbeNO7I6AfS4dquG'
+    config.access_token = 'p10WCLlInJwWtVWBraB4VANUVzD9dEJxItwVxM6fiHVkaHZNcD'
     config.host = 'api-uat.dwolla.com'
     config.base_path = '/'
     config.verify_ssl = false if Rails.env.development?
